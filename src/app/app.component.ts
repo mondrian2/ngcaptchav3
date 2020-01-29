f32bfa6a0da385e780ec0a066d67091932ee8fb2
Daniel Faustino Silva <deecomunicacao@gmail.com>
2020-01-29 07:30:25 -0300

    compnent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'v3captcha';
+export class AppComponent implements OnInit {
+ 
+  @ViewChild('recaptcha', {static: true }) recaptchaElement: ElementRef;
+ 
+  constructor( private http: HttpClient ) { }
+ 
+  ngOnInit() {
+    this.addRecaptchaScript();
+  }
+
+  response: any;
+ 
+  renderReCaptch() {
+    window['grecaptcha'].render(this.recaptchaElement.nativeElement, {
+      'sitekey' : '6LfcytIUAAAAAKofzqGYVEfLGGjj6tPjiMkLGAFw',
+      'callback': (response) => {
+          console.log(response);
+          this.response = response;
+      }
+    });
+  }
+
+  enviar(){
+    this.http.post('https://www.google.com/recaptcha/api/siteverify', {
+            'secret': '6LfcytIUAAAAAKS9q_SOtXIbwhZmFlI8gVH4FwxZ',
+            'response': this.response
+          }).subscribe(r => console.log(r));
+  }
+ 
+  addRecaptchaScript() {
+ 
+    window['grecaptchaCallback'] = () => {
+      this.renderReCaptch();
+    }
+ 
+    (function(d, s, id, obj){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { obj.renderReCaptch(); return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://www.google.com/recaptcha/api.js?onload=grecaptchaCallback&amp;render=explicit";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'recaptcha-jssdk', this));
+ 
+  }
+ 
 }
